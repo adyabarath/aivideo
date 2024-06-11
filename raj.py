c2ccff0b886d923804ed70c23591d10bf75bd217
adyabarath <barath.raj@adya.ai>
2024-06-12 00:48:07 +0530

    Update raj.py

--- raj.py
+++ raj.py
@@ -13,7 +13,7 @@ from moviepy.editor import ImageClip, AudioFileClip, VideoFileClip, concatenate_
 from moviepy.video.tools.subtitles import SubtitlesClip
 import numpy as np
 import re
-
+change_settings({"IMAGEMAGICK_BINARY": "convert.exe"})
 def time_to_seconds(time_obj):
     return time_obj.hours * 3600 + time_obj.minutes * 60 + time_obj.seconds + time_obj.milliseconds / 1000
 
