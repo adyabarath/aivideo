0c7af1037fe2aa122a70e4b59460df989c8a686a
adyabarath <barath.raj@adya.ai>
2024-06-13 09:55:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-
-# Install ImageMagick
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
 apt-get update && apt-get install -y imagemagick
